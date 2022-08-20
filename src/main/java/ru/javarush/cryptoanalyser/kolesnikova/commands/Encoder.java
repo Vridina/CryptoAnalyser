16e2fec7268f3133cb90a126312fd0f410f96afa
Ekaterina <kate.ko3452@yandex.ru>
2022-08-20 21:52:50 +0500

    add 2.3 // сделала кодирование данных, без вывода

--- src/main/java/ru/javarush/cryptoanalyser/kolesnikova/commands/Encoder.java
+++ src/main/java/ru/javarush/cryptoanalyser/kolesnikova/commands/Encoder.java
@@ -11,32 +11,28 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+import static ru.javarush.cryptoanalyser.kolesnikova.constants.Strings.ALPHABET;
+
 public class Encoder implements Action {
     @Override
     public Result execute(String[] parameters) {
         Path readPathFirstFile = Action.readPathFirstFile(parameters);
         try {
             List<String> strings = Files.readAllLines(readPathFirstFile);
-            String takeString = strings.get(0);
-            char takeChar = takeString.charAt(0);
             int key = Integer.parseInt(parameters[2]);
-            char[] alfabet = Strings.ALPHABET.toCharArray();
-
-
-
-            // проверить содежится ли символ в алфавите
-            // если да то заменить
-            //заменить как - найти текущий индек полученого символа и прибавить к нему + кей, то есть по сути у меня
-            // стринг Алфавитов должен быть преобразован в массив с индексами
-            // если нет то ничего не делать - континуе.
-
-
-            System.out.println("\n\n\n_____попытка понять v____");
-            System.out.println(strings.get(0));
-            System.out.println(strings.size());
-            System.out.println(alfabet);
-
-
+            for (int i = 0; i < strings.size(); i++) {
+                String takeString = strings.get(i);
+//                System.out.println("Строка № " + i + " : " + takeString);
+                char[] takeCharAr = takeString.toCharArray();
+                for (int j = 0; j < takeCharAr.length; j++) {
+                    if (ALPHABET.contains(Character.toLowerCase(takeCharAr[j]))) {
+                        int IndexSymbol = ALPHABET.indexOf(Character.toLowerCase(takeCharAr[j]));
+                        int resaltIndex = (IndexSymbol + key) % ALPHABET.size();
+                        takeCharAr[j] = ALPHABET.get(resaltIndex);
+                    }
+                }
+//                System.out.println(takeCharAr);
+            }
         } catch (
                 IOException e) {
             throw new ApplicationExceptions("IO error", e);
@@ -44,7 +40,31 @@ public class Encoder implements Action {
         return new Result(ResultCode.OK, "Файл успешно прочитан. Адрес файла: " + readPathFirstFile);
 
 
+//                    ПОДНЯТЬ ДО ТОГО КАК МЕНЯЮ СИМВОЛЫ vvvvv
+//                    System.out.println("Вот это есть в алфавите ->  " + symbols);
+//                    System.out.println("Старый индекс(IndexSymbol) ->" + IndexSymbol);
+//                    System.out.println("НОВЫЙ индекс(resaltIndex)->" + resaltIndex);
+//                    System.out.println("Новый символ теперь это --- " + symbols);
+//                    System.out.println("_________________");
 
+
+//            }
+        // TODO - доработать заглавные буквы, сейчас я их теряю при шифровании
+        // TODO - доработать что делать, если символа нет в алфавите
+        // я одна мега тупая и ничо не понимаюююююююююююююююю, ааааааааааааааааа!!! почему так сложно)))))
+        // и эта тема по потоам меня УБИВАЕТ
+        // проверить содежится ли символ в алфавите
+        // если да то заменить
+        //заменить как - найти текущий индек полученого символа и прибавить к нему + кей, то есть по сути у меня
+        // стринг Алфавитов должен быть преобразован в массив с индексами
+        // если нет то ничего не делать - континуе.
+
+//
+//            System.out.println("\n\n\n_____попытка понять v____");
+//            System.out.println(strings.get(0));
+//            System.out.println(strings.size());
+//            System.out.println("Длина алфавита " + ALPHABET.size());
+////            System.out.println(alfabet);
 
         /*
         1. чтение пути файла - метод - ок
--- src/main/java/ru/javarush/cryptoanalyser/kolesnikova/constants/Strings.java
+++ src/main/java/ru/javarush/cryptoanalyser/kolesnikova/constants/Strings.java
@@ -1,11 +1,24 @@
 package ru.javarush.cryptoanalyser.kolesnikova.constants;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Strings {
-    private static final String RU = "אבגדהו¸זחטיךכלםמןנסעףפץצקרשתûü‎‏ÿ";
-//    private static final String EN = "abcdefghijklmnopqrstuvwxyz";
-    private static final String NUMBERS = "1234567890";
-    private static final String SYMBOLS = " .,«»\"'\\:!? +-=&@*()";
-    public static final String ALPHABET = RU + RU.toUpperCase()  + SYMBOLS + NUMBERS;
+//    private static final List<Character> RU = Arrays.asList('א', 'ב', 'ג',
+//            'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'ך', 'כ', 'ל', 'ם', 'מ', 'ן', 'נ', 'ס', 'ע', 'ף',
+//            'פ', 'ץ', 'צ', 'ק', 'ר', 'ש', 'ת', 'û', 'ü', '‎', 'ÿ');
+//
+//        private static final String EN = "abcdefghijklmnopqrstuvwxyz";
+//    private static final List<Character> NUMBERS = Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+//    private static final String SYMBOLS = " .,«»\"'\\:!? +-=&@*()";
+//    public static final List<Character> ALPHABET =
+
+    public static final List<Character> ALPHABET = Arrays.asList('א', 'ב', 'ג',
+            'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'ך', 'כ', 'ל', 'ם', 'מ', 'ן', 'נ', 'ס', 'ע', 'ף',
+            'פ', 'ץ', 'צ', 'ק', 'ר', 'ש', 'ת', 'û', 'ü', '‎', 'ÿ', '.', ',', '«', '»',
+            ':', '!', '?', ' ');
+
 
 
 }
